2af983086376f34f8466caef7e18444c3df6dd79
Domapes <d.schmidt@clevervital.com>
2022-08-10 16:19:11 +0200

    Final Commit, build finished online store application with a product catalog, a shopping cart, and a checkout function.

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Product } from './products';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
   items: Product[] = [];
 
@@ -21,5 +25,9 @@ export class CartService {
   clearCart() {
     this.items = [];
     return this.items;
+  }
+
+  getShippingPrices(){
+    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
   }
 }
